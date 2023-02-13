a3c038b3e81718acaba67ec32c73d3b15fae8ae2
Crowdbotics <team@crowdbotics.com>
2023-02-13 02:15:12 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chet_s_38870/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chet_s_38870;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "chet_s_38870";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chet-s-38870",
+  url: "https://chet-s-38870.botics.co",
+  api: "https://chet-s-38870.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
